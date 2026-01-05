6c97b4094284e2d1c2eee60f22b848011654f97f
eliemine24 <elie.gautier24@gmail.com>
2026-01-05 16:33:53 +0100

    suppr perlin.go

--- matrice.go
+++
@@ -1,7 +0,0 @@
-// ==============================
-// === Génération de matrices ===
-// ==============================
-
-// Go routine pour la génration de matrices avec bruit de perlin
-// initialisation d'une matrice de taille NxN
-// remplissage avec des valeurs comprises entre 0 et 1 (altitude)
