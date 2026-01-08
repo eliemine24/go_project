3a03a113b4ab03059860f47e028dd630aab67ad3
eliemine24 <elie.gautier24@gmail.com>
2026-01-08 09:36:27 +0100

    aled

--- main.go
+++ main.go
@@ -5,52 +5,39 @@
 package main
 
 import (
-	"image/color"
-
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/palette/moreland"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg"
+	"gns/matrix"
 )
 
-// matrice utilisée par main() pour l'affichage (evite variable non définie dans main)
-var matrice [][]float64
+const (
+	MAPSIZE      = 10                               // taille des maps elementaires
+	RATIO        = 10                               // rapport de la taille de la map finale par la taille des maps elementaires
+	FINALMAPSIZE = MAPSIZE * RATIO                  // taille de la map finale
+	MAPNB        = FINALMAPSIZE / MAPSIZE           // nombre de map elementaires sur la map finale
+	FINALMAP     = matrix.InitMatrice(FINALMAPSIZE) // Map finale (matrice carrée de flottant)
+)
 
-// Affichage graphique d'une matrice carrée de taille n contenant des flottants entre 0 et 1
-func displayMat(matrice [][]float64) {
+// canal pour la génération des matrices bruitées
+c := make(chan [][]float64)
+
+func GenerateNoisedMap(size int, out <- [][]float64){
+	
+}
+
+func LauchGeneration(n int, c chan){
+	for(N){
+		c <- GenerateElemMap(size)
+	}
 
 }
 
-// ============
-// === MAIN ===
-// ============
-// Fonction principale génère la carte finale et l'affiche
 func main() {
+	// Générer les matrices et générer perlin dessus
+	// alimenter le canal avec la go routine de génération de perlin noise
 
-	p := plot.New()
+	// func Worker(canal){
+	// for (j in range c)
+	// do Job(j) }
 
-	p.X.Min = 0
-	p.X.Max = TAILLE
-	p.Y.Min = 0
-	p.Y.Max = TAILLE
+	// for (number <K){go Worker}
 
-	p.X.Tick.Marker = plot.ConstantTicks([]plot.Tick{})
-	p.Y.Tick.Marker = plot.ConstantTicks([]plot.Tick{})
-
-	p.X.LineStyle.Width = 0
-	p.Y.LineStyle.Width = 0
-
-	cm := moreland.Kindlmann()
-	cm.SetMax(1)
-	cm.SetMin(0)
-
-	palette := cm.Palette(255)
-	hm := plotter.NewHeatMap(HeatmapData(matrice), palette)
-	hm.NaN = color.Transparent
-
-	p.Add(hm)
-
-	if err := p.Save(6*vg.Inch, 6*vg.Inch, "perlin_heatmap.png"); err != nil {
-		panic(err)
-	}
 }
