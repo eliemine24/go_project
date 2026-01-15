0b28475c8d46007828e40fa39abcc83c36e70b24
eliemine24 <elie.gautier24@gmail.com>
2026-01-15 15:51:41 +0100

    script pour l'install (linux)

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#bin/bash
+
+go mod init go_project
+
+# ressources for l'affichage de la map
+go get gonum.org/v1/plot
+go get gonum.org/v1/plot/palette/moreland
+go get gonum.org/v1/plot/plotter
+go get gonum.org/v1/plot/vg
